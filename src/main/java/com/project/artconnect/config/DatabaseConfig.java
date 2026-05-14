65d4d4fded1de18df3f7f1930df16dcaafb19254
Flogix <flogiraud74@gmail.com>
2026-05-14 11:00:47 +0200

    test florian

--- src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -8,5 +8,5 @@ public class DatabaseConfig {
     // The dump shows the database is named "artconnect"
     public static final String URL = "jdbc:mysql://localhost:3306/artconnect?serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASSWORD = "Tenshi:94"; // CHANGE ME if you have a password
+    public static final String PASSWORD = "Florian2005!"; // CHANGE ME if you have a password
 }
